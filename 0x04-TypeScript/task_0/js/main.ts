9f6f54a6b51254fc34bcff01ca5b15dd48335c18
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2024-10-09 17:33:25 -0700

    typeScript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,40 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 20,
+  location: "New York"
+};
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Smith",
+  age: 22,
+  location: "Los Angeles"
+};
+
+const studentsList: Student[] = [student1, student2];
+
+const table = document.createElement("table");
+document.body.appendChild(table);
+
+studentsList.forEach((student) => {
+  const row = document.createElement("tr");
+
+  const firstNameCell = document.createElement("td");
+  firstNameCell.textContent = student.firstName;
+  row.appendChild(firstNameCell);
+
+  const locationCell = document.createElement("td");
+  locationCell.textContent = student.location;
+  row.appendChild(locationCell);
+
+  table.appendChild(row);
+});
+
